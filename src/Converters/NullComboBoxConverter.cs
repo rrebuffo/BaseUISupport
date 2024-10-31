05cd43142ecd028769ac31f2f04eb012ddc784ab
Mauro Rebuffo <mauro@mehir.com.ar>
2024-10-31 04:42:36 -0300

    Add converter to allow nullable ComboBoxes

---
+++ src/Converters/NullComboBoxConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BaseUISupport.Converters;
+
+public class NullComboBoxConverter : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value;
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is string str && parameter is string param && str == param) return null;
+        else return value;
+    }
+}
