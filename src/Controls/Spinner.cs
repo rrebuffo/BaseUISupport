ba18af4ff9a28d8580e524c0534210647561a44d
Mauro Rebuffo <mauro@mehir.com.ar>
2025-02-09 14:44:26 -0300

    Add basic Spinner control

---
+++ src/Controls/Spinner.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BaseUISupport.Controls;
+
+public class Spinner : Control
+{
+    public bool IsLoading
+    {
+        get { return (bool)GetValue(IsLoadingProperty); }
+        set { SetValue(IsLoadingProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for IsLoading.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IsLoadingProperty =
+        DependencyProperty.Register("IsLoading", typeof(bool), typeof(Spinner), new PropertyMetadata(false));
+
+    static Spinner()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(Spinner), new FrameworkPropertyMetadata(typeof(Spinner)));
+    }
+}
